ca4f65b050f21fdc0c1c2c5666ab864b539235be
gpunit2417 <punitgoyal106@gmail.com>
2025-03-10 21:05:44 +0530

    backend main file

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes.js";
+import router from "./routes/user-routes.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
+    "mongodb+srv://punitgoyal106:J3hMOXyBqpySnGmY@cluster0.n5o5m.mongodb.net/"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
+
+
+  //J3hMOXyBqpySnGmY
